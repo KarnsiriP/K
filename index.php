ab9da9d2fd22835a870ce94086474e145d456809
KarnsiriP <73680631+KarnsiriP@users.noreply.github.com>
2020-10-30 11:43:41 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+    <head> 
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1> เเนะนำตัว </h1>
+        <p> กานต์สิริ พงษ์ศิยานุวัฒน์  ชื่อเล่น ：พลอย </p>
+        <img src="https://www.thephuketnews.com/photo/listing/2019/1565765176_1.jpg">
+        <img src="https://cdn.pixabay.com/photo/2017/09/24/08/38/cat-2781169_960_720.jpg">
+        <br>
+        <a href="https://www.instagram.com/cats_of_instagram/?hl=en">Pls come to this link</a>
+        
+        <h2> เรื่องตลก </h2>
+        <p> ท่านผู้โดยสารทุกท่านโปรดทราบขณะนี้เครื่องบินของเราที่จะบินไปปารากวัย</p>
+        <p> พร้อมที่จะออกเดินทางแล้วคะ และขอให้ผู้โดยสารทุกท่านวางใจในสายการบินของเราได้</p> 
+        <p>  เพราะถึงแม้ว่าเครื่องบินของเราจะเก่าแต่นักบินของเราก็ยังใหม่อยู่คะ </p>
+    <body>
+
+
+
+</html>
